1c993e290e96e57534e44857540aa17973029384
Ilyas <zone_ender1994@hotmail.com>
2022-09-15 11:49:39 +0200

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link> |
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
+  </div>
+
+  <button @click="Redirect">Redirect</button>
+  <button @click="Back">Go Back</button>
+  <button @click="Forward">Go Forward</button>
+
+  <router-view/>
+</template>
+
+<script>
+export default {
+  methods: {
+    Redirect() {
+      this.$router.push({ name: 'Home'})
+    },
+    Back() {
+      this.$router.go(-1)
+    },
+    Forward() {
+      this.$router.go(+1)
+    }
+  }
+}
+
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+#nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
+  padding: 8px;
+  border-radius: 4px;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum fuga mollitia praesentium impedit ipsa. Alias voluptatem commodi deserunt officia placeat, beatae esse dolore neque voluptatibus. Reprehenderit rem, est ratione nostrum ipsa at ullam? Error alias in explicabo reiciendis itaque. Perspiciatis pariatur, optio voluptas ipsum reprehenderit quibusdam at consectetur adipisci officiis unde modi deserunt explicabo saepe accusantium molestiae neque itaque? In officiis quasi alias quidem. Tempore nisi recusandae unde, et sunt libero culpa ab quia ex porro itaque ea nam in. Eaque molestiae mollitia voluptas minus unde ipsa corrupti, at id ab, quia, quibusdam nam explicabo culpa non provident eum magnam.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum fuga mollitia praesentium impedit ipsa. Alias voluptatem commodi deserunt officia placeat, beatae esse dolore neque voluptatibus. Reprehenderit rem, est ratione nostrum ipsa at ullam? Error alias in explicabo reiciendis itaque. Perspiciatis pariatur, optio voluptas ipsum reprehenderit quibusdam at consectetur adipisci officiis unde modi deserunt explicabo saepe accusantium molestiae neque itaque? In officiis quasi alias quidem. Tempore nisi recusandae unde, et sunt libero culpa ab quia ex porro itaque ea nam in. Eaque molestiae mollitia voluptas minus unde ipsa corrupti, at id ab, quia, quibusdam nam explicabo culpa non provident eum magnam.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {  }
+}
+</script>
